f0519229075acabfb2b83579349849e238cbf41a
TianHuan <tianhuan@bingotree.cn>
2015-07-14 15:34:48 +0800

    create setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sflow_parser",
+    version="0.10",
+    description="sFlow Parser Tool",
+    author="Qin TianHuan",
+    author_email="tianhuan@bingotree.cn",
+    url="bingotree.cn",
+    packages=find_packages(),
+)
