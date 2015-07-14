1576e687bd74611fd7dfde42218c88b28a9b728c
TianHuan <tianhuan@bingotree.cn>
2015-07-14 10:22:19 +0800

    working on counter recode

--- sflow_parser/__init__.py
+++ sflow_parser/__init__.py
@@ -8,6 +8,13 @@ from xdrlib import Unpacker
 FORMAT_FLOW_SAMPLE = 1
 FORMAT_COUNTER_SAMPLE = 2
 
+FORMAT_COUNTER_RECORD_GENERIC = 1
+FORMAT_COUNTER_RECORD_ETHERNET = 2
+FORMAT_COUNTER_RECORD_TOKENRING = 3
+FORMAT_COUNTER_RECORD_100BASEVG = 4
+FORMAT_COUNTER_RECORD_VLAN = 5
+FORMAT_COUNTER_RECORD_PROCESS = 1001
+
 
 class SFlowPacket(object):
 
@@ -41,7 +48,108 @@ class CounterRecord(Record):
 
     def __init__(self, sample_data):
         super(self, CounterRecord).__init__(sample_data)
-        # TODO
+        self.format = sample_data.unpack_uint()
+        record_data = Unpacker(sample_data.unpack_opaque())
+
+        if self.format == FORMAT_COUNTER_RECORD_GENERIC:
+            self._parse_generic(record_data)
+        elif self.format == FORMAT_COUNTER_RECORD_ETHERNET:
+            self._parse_ethernet(record_data)
+        elif self.format == FORMAT_COUNTER_RECORD_TOKENRING:
+            self._parse_tokenring(record_data)
+        elif self.format == FORMAT_COUNTER_RECORD_100BASEVG:
+            self._parse_100basevg(record_data)
+        elif self.format == FORMAT_COUNTER_RECORD_VLAN:
+            self._parse_vlan(record_data)
+        elif self.format == FORMAT_COUNTER_RECORD_PROCESS:
+            self._parse_process(record_data)
+
+    def _parse_generic(self, record_data):
+        self.index = record_data.unpack_uint()
+        self.if_type = record_data.unpack_uint()
+        self.speed = record_data.unpack_uhyper()
+        self.direction = record_data.unpack_uint()
+        self.status = record_data.unpack_uint()
+        self.in_octets = record_data.unpack_uhyper()
+        self.in_ucasts = record_data.unpack_uint()
+        self.in_mcasts = record_data.unpack_uint()
+        self.in_bcasts = record_data.unpack_uint()
+        self.in_discards = record_data.unpack_uint()
+        self.in_errors = record_data.unpack_uint()
+        self.in_unknown_protos = record_data.unpack_uint()
+        self.out_octets = record_data.unpack_uhyper()
+        self.out_ucasts = record_data.unpack_uint()
+        self.out_mcasts = record_data.unpack_uint()
+        self.out_bcasts = record_data.unpack_uint()
+        self.out_discards = record_data.unpack_uint()
+        self.out_errors = record_data.unpack_uint()
+        self.promiscuous_mode = record_data.unpack_uint()
+
+    def _parse_ethernet(self, record_data):
+        self.dot3StatsAlignmentErrors = record_data.unpack_uint()
+        self.dot3StatsFCSErrors = record_data.unpack_uint()
+        self.dot3StatsSingleCollisionFrames = record_data.unpack_uint()
+        self.dot3StatsMultipleCollisionFrames = record_data.unpack_uint()
+        self.dot3StatsSQETestErrors = record_data.unpack_uint()
+        self.dot3StatsDeferredTransmissions = record_data.unpack_uint()
+        self.dot3StatsLateCollisions = record_data.unpack_uint()
+        self.dot3StatsExcessiveCollisions = record_data.unpack_uint()
+        self.dot3StatsInternalMacTransmitErrors = record_data.unpack_uint()
+        self.dot3StatsCarrierSenseErrors = record_data.unpack_uint()
+        self.dot3StatsFrameTooLongs = record_data.unpack_uint()
+        self.dot3StatsInternalMacReceiveErrors = record_data.unpack_uint()
+        self.dot3StatsSymbolErrors = record_data.unpack_uint()
+
+    def _parse_tokenring(self, record_data):
+        self.dot5StatsLineErrors = record_data.unpack_uint()
+        self.dot5StatsBurstErrors = record_data.unpack_uint()
+        self.dot5StatsACErrors = record_data.unpack_uint()
+        self.dot5StatsAbortTransErrors = record_data.unpack_uint()
+        self.dot5StatsInternalErrors = record_data.unpack_uint()
+        self.dot5StatsLostFrameErrors = record_data.unpack_uint()
+        self.dot5StatsReceiveCongestions = record_data.unpack_uint()
+        self.dot5StatsFrameCopiedErrors = record_data.unpack_uint()
+        self.dot5StatsTokenErrors = record_data.unpack_uint()
+        self.dot5StatsSoftErrors = record_data.unpack_uint()
+        self.dot5StatsHardErrors = record_data.unpack_uint()
+        self.dot5StatsSignalLoss = record_data.unpack_uint()
+        self.dot5StatsTransmitBeacons = record_data.unpack_uint()
+        self.dot5StatsRecoverys = record_data.unpack_uint()
+        self.dot5StatsLobeWires = record_data.unpack_uint()
+        self.dot5StatsRemoves = record_data.unpack_uint()
+        self.dot5StatsSingles = record_data.unpack_uint()
+        self.dot5StatsFreqErrors = record_data.unpack_uint()
+
+    def _parse_100basevg(self, record_data):
+        self.dot12InHighPriorityFrames = record_data.unpack_uint()
+        self.dot12InHighPriorityOctets = record_data.unpack_uhyper()
+        self.dot12InNormPriorityFrames = record_data.unpack_uint()
+        self.dot12InNormPriorityOctets = record_data.unpack_uhyper()
+        self.dot12InIPMErrors = record_data.unpack_uint()
+        self.dot12InOversizeFrameErrors = record_data.unpack_uint()
+        self.dot12InDataErrors = record_data.unpack_uint()
+        self.dot12InNullAddressedFrames = record_data.unpack_uint()
+        self.dot12OutHighPriorityFrames = record_data.unpack_uint()
+        self.dot12OutHighPriorityOctets = record_data.unpack_uhyper()
+        self.dot12TransitionIntoTrainings = record_data.unpack_uint()
+        self.dot12HCInHighPriorityOctets = record_data.unpack_uhyper()
+        self.dot12HCInNormPriorityOctets = record_data.unpack_uhyper()
+        self.dot12HCOutHighPriorityOctets = record_data.unpack_uhyper()
+
+    def _parse_vlan(self, record_data):
+        self.vlan_id = record_data.unpack_uint()
+        self.octets = record_data.unpack_uhyper()
+        self.ucastPkts = record_data.unpack_uint()
+        self.multicastPkts = record_data.unpack_uint()
+        self.broadcastPkts = record_data.unpack_uint()
+        self.discards = record_data.unpack_uint()
+
+    def _parse_process(self, record_data):
+        self.cpu_percentage_in_5s = record_data.unpack_uint()
+        self.cpu_percentage_in_1m = record_data.unpack_uint()
+        self.cpu_percentage_in_5m = record_data.unpack_uint()
+        self.total_memory = record_data.unpack_hyper()
+        self.free_memory = record_data.unpack_hyper()
 
 
 class Sample(object):
